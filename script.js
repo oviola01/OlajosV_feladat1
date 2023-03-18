25f0bf5477568cd1159f2f822f195efeae281549
Olajos Viola (SZF_E_2022_2024) <olajos.viola@diak.szamalk-szalezi.hu>
2023-03-18 09:23:58 +0100

    javítás

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ window.addEventListener("load", init)
 const kutyaLista=["foxterrier","kuvasz","puli","agár","újfullandi","keverék"]
 
 function init() {
-    button.addEventListener("click", article.innerHTML += <p>kutyaLista[2]</p>);
+    const ARTIC = document.querySelector("article");
+    ARTIC.innerHTML += "<button>Kutya</button>";
+    const GOMB = document.querySelector("button");
+    GOMB.addEventListener("click", function() {kattintas(ARTIC)});
 }
+
+function kattintas(hova) {
+    hova.innerHTML += `<p>${kutyaLista[2]}</p>`;
+} 
