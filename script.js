d0cd1d51a5c8bfb5033795d45eafd770706a1b57
Olajos Viola (SZF_E_2022_2024) <olajos.viola@diak.szamalk-szalezi.hu>
2023-03-18 08:54:10 +0100

    dolgozat

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", init)
+
+const kutyaLista=["foxterrier","kuvasz","puli","agár","újfullandi","keverék"]
+
+function init() {
+    button.addEventListener("click", article.innerHTML += <p>kutyaLista[2]</p>);
+}
